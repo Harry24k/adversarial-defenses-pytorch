5c283779ed9b0bd698b47311dec81d39a7796a91
Harry Kim <24K.Harry@gmail.com>
2021-06-10 15:58:47 +0900

    Update adv_trainer.py

--- defenses/trainers/adv_trainer.py
+++ defenses/trainers/adv_trainer.py
@@ -19,35 +19,40 @@ class AdvTrainer(Trainer):
         super(AdvTrainer, self).__init__(name, model)
         self._flag_record_rob = False
     
-    def record_rob(self, train_set, test_set, eps, alpha, steps):
+    def record_rob(self, train_loader, val_loader, eps, alpha, steps, sigma=0.1, num_limit=1000):
         self.record_keys += ['Clean(Tr)', 'FGSM(Tr)', 'PGD(Tr)', 'GN(Tr)',
-                             'Clean(Te)', 'FGSM(Te)', 'PGD(Te)', 'GN(Te)',]
-        
-        self.train_set = train_set
-        self.test_set = test_set
+                             'Clean(Val)', 'FGSM(Val)', 'PGD(Val)', 'GN(Val)',]
         self.record_atks = [VANILA(self.model), FGSM(self.model, eps=eps),
                             PGD(self.model, eps=eps, alpha=alpha, steps=steps),
-                            GN(self.model, sigma=0.1)]
+                            GN(self.model, sigma=sigma)]
+        
+        self.train_loader_rob = train_loader
+        self.val_loader_rob = val_loader
         self._flag_record_rob = True
+        self.num_limit_rob = num_limit
     
     # Update Records
     def _update_record(self, records):
         if self._flag_record_rob:
             adv_list = []
             for atk in self.record_atks:
-                adv_list.append([(atk(*self.train_set), self.train_set[1])])
+                acc = get_acc(self.model, atk, self.train_loader_rob,
+                              device=self.device, num_limit=self.num_limit_rob)
+                adv_list.append(acc)
             for atk in self.record_atks:
-                adv_list.append([(atk(*self.test_set), self.test_set[1])])
+                acc = get_acc(self.model, atk, self.val_loader_rob,
+                              device=self.device, num_limit=self.num_limit_rob)
+                adv_list.append(acc)
             
             self.rm.add([*records,
-                         *[get_acc(self.model, adv_data) for adv_data in adv_list],
+                         *adv_list,
                          self.optimizer.param_groups[0]['lr']])
         else:
             self.rm.add([*records,
                          self.optimizer.param_groups[0]['lr']])
                          
                 
-def get_acc(model, test_loader, device='cuda'):
+def get_acc(model, atk, data_loader, device='cuda', num_limit=None):
     # Set Cuda or Cpu
     device = torch.device(device)
     model.to(device)
@@ -60,18 +65,23 @@ def get_acc(model, test_loader, device='cuda'):
     total = 0
 
     # For all Test Data
-    for batch_images, batch_labels in test_loader:
+    for batch_images, batch_labels in data_loader:
 
         # Get Batches
         X = batch_images.to(device)
         Y = batch_labels.to(device)
         
         # Forward
-        pre = model(X)
+        X_adv = atk(X, Y)
+        pre = model(X_adv)
 
         # Calculate Accuracy
         _, pre = torch.max(pre.data, 1)
         total += pre.size(0)
         correct += (pre == Y).sum()
+        
+        if num_limit is not None:
+            if total > num_limit:
+                break
 
     return (100 * float(correct) / total)
