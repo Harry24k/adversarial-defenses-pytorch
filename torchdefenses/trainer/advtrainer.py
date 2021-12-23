b06d2f1517c3b02d5ba098ecf7ca73f0379808dc
Harry Kim <24K.Harry@gmail.com>
2021-12-23 11:48:40 +0900

    v0.1.13

--- torchdefenses/trainer/advtrainer.py
+++ torchdefenses/trainer/advtrainer.py
@@ -16,7 +16,7 @@ class AdvTrainer(Trainer):
         super(AdvTrainer, self).__init__(name, model)
         self._flag_record_rob = False
     
-    def record_rob(self, train_loader, val_loader, eps, alpha, steps, std=0.1, n_limit=1000):
+    def record_rob(self, train_loader, val_loader, eps, alpha, steps, std=0.1):
         self.record_keys += ['Clean(Tr)', 'FGSM(Tr)', 'PGD(Tr)', 'GN(Tr)',
                              'Clean(Val)', 'FGSM(Val)', 'PGD(Val)', 'GN(Val)',]    
         self._flag_record_rob = True    
@@ -26,26 +26,23 @@ class AdvTrainer(Trainer):
         self._alpha_rob = alpha
         self._steps_rob = steps
         self._std_rob = std
-        self._n_limit_rob = n_limit
     
     # Update Records
     def _update_record(self, records):
         if self._flag_record_rob:
             rob_records = []
             for loader in [self._train_loader_rob, self._val_loader_rob]:
-                rob_records.append(self.model.eval_accuracy(loader,
-                                                            n_limit=self._n_limit_rob))
+                rob_records.append(self.model.eval_accuracy(loader))
                 rob_records.append(self.model.eval_rob_accuracy_fgsm(loader,
-                                                                     eps=self._eps_rob,
-                                                                     n_limit=self._n_limit_rob))
+                                                                     eps=self._eps_rob, verbose=False)
                 rob_records.append(self.model.eval_rob_accuracy_pgd(loader,
                                                                     eps=self._eps_rob,
                                                                     alpha=self._alpha_rob,
                                                                     steps=self._steps_rob,
-                                                                    n_limit=self._n_limit_rob))
+                                                                    verbose=False))
                 rob_records.append(self.model.eval_rob_accuracy_gn(loader,
                                                                    std=self._std_rob,
-                                                                   n_limit=self._n_limit_rob))
+                                                                   verbose=False))
             
             self.rm.add([*records,
                          *rob_records,
