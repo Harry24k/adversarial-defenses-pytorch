729600f46bafd396d932acdf2332258ddec9ae9a
Harry Kim <24K.Harry@gmail.com>
2022-01-18 14:50:29 +0900

    v0.1.21

--- torchdefenses/utils/models/resnet.py
+++ torchdefenses/utils/models/resnet.py
@@ -64,11 +64,11 @@ class ResBottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, in_channels=3):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
